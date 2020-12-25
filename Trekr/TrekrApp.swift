b5ebfa6ae2f40d835e0c884858cb7e5e1e78f56e
Conrad Taylor <conradwt@gmail.com>
2020-12-25 14:25:59 -0800

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Conrad Taylor on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
